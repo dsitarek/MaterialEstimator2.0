bb79ee374b5212f4eea5f24d60c6363c88288e40
Daniel Sitarek <deltasierra777@gmail.com>
2022-02-09 22:05:14 -0600

    added material table with material type list connected to db for type table

--- src/components/MaterialRow.js
+++ src/components/MaterialRow.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+// import { getUser } from '../api/auth';
+import getMaterialTypes from '../data/dbCalls';
 
 export default function MaterialRow({ material }) {
+  const [materialTypes, setMaterialTypes] = useState([]);
+  const [formInput, setFormInput] = useState({ materialPN: '' });
+
+  const handleChange = (e) => {
+    setFormInput((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
+  };
+
+  useEffect(() => {
+    getMaterialTypes().then(setMaterialTypes);
+  }, []);
   return (
     <>
       <tr>
@@ -12,7 +24,9 @@ export default function MaterialRow({ material }) {
           {material.matQuantity}
         </th>
         <th>
-          Material
+          <select id="materialType" name="materialType" value={formInput.materialPN} aria-labelledby="material-type" onChange={handleChange}>
+            {materialTypes.map((mat) => <option key={mat.PartNumber} value={mat.PartNumber}>{mat.Description}</option>)}
+          </select>
         </th>
       </tr>
     </>
